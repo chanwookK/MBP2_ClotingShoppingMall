6b342ed51595ca1c6045b3f5de7eb2274e848f18
SonSuBin129 <ttokky0129@gmail.com>
2023-10-20 11:31:13 +0900

    [관리자] Test용

--- src/ManagerMain.java
+++ src/ManagerMain.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class ManagerMain {
