0476fa02c25316bb209665fa6c8823f3d7dfa707
sam <10502sam@gmail.com>
2023-10-28 13:27:39 +0900

    User 구현 완료

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         //관리자 테스트용으로 작성
-//        User user= new User();
-//        ManagerMain manager= new ManagerMain(user);
+        //유저 테스트용 작성
+
+        //User user= new User();
+        //ManagerMain manager= new ManagerMain(user);
+        //UserMain userMain = new UserMain(user);
+
 
 
     }
