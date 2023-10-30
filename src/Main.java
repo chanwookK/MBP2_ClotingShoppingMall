b7d564c4e437a8add10ab3259d5b4c540f6bf0c2
sam <10502sam@gmail.com>
2023-10-31 02:51:55 +0900

    [User] showMyPage() getCoupon Map으로 수정

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        //ProgramMain main = new ProgramMain();
+//        ProgramMain main = new ProgramMain();
 
         //Manager test용
 //        User user = new User("관리자","adminadminadmin","pass00",0);
 //        ManagerMain manager=new ManagerMain(user);
+
+        //User test용
+//        Map<String, Integer> myMap = new HashMap<>();
+//        myMap.put("5000원", 3);
+//        User user = new User("곽민재", "thisisId", "thisisPassword",  myMap);
+//        UserMain userMain = new UserMain(user);
 
     }
 }
